a8702d640302bf4f4d310b708280192e7ee94d82
Donald <ediagee@yahoo.com>
2021-10-09 12:10:40 +0300

    added header component and styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="app">Hello donald...</div>
+      <Header />
+      <div className="app"></div>
     </Provider>
   );
 }
 
 export default App;
+
+// git branch -D branch-name
+// git checkout -b branchName
